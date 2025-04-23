914bf5f7393a4c401989495c4e04b1e2f99ec61d
Danylo-D87 <quaestor88@gmail.com>
2025-04-23 14:13:36 +0300

    sol changed

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ class Car:
                  clean_mark: int,
                  brand: str
                  ) -> None:
-
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
